00c75666815497b3ab777ede99a9be31377f4b1a
Aditya <noxx.music27@gmail.com>
2025-11-09 09:57:07 -0500

    added crystal logo to header

--- components/header.tsx
+++ components/header.tsx
@@ -16,7 +16,7 @@ export function Header() {
         <div className="flex h-16 items-center justify-between">
           {/* Logo */}
           <div className="flex items-center gap-2">
-
+            <img src="/crystal.png" alt="Crystal Logo" className="h-8 w-8" />
             <span className="text-xl font-bold text-foreground">Quartz</span>
           </div>
 
